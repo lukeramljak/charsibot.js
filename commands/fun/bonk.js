b0efbf79549899b3e66a8fc6603d7b6cc48f3cda
Luke Ramljak <lukeramljak@proton.me>
2023-11-17 23:26:59 +0800

    add missing description

--- commands/fun/bonk.js
+++ commands/fun/bonk.js
@@ -4,10 +4,12 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('bonk')
     .setDescription('Bonk someone')
-    .addUserOption((option) => option.setName('name').setRequired(true)),
+    .addUserOption((option) =>
+      option.setName('name').setDescription('user to bonk').setRequired(true)
+    ),
   category: 'fun',
   async execute(interaction) {
-    const target = interaction.options.getUser('target');
+    const target = interaction.options.getUser('name');
     await interaction.reply(
       `${interaction.user.displayName} has bonked <@${target.id}>. Oh my...`
     );
--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('reload')
-    .setDescription('Reloads a command.')
+    .setDescription('Reloads a command')
     .addStringOption((option) =>
       option
         .setName('command')
