b84a171aad63793910e120fba044907af7053a23
Luke Ramljak <lukeramljak@proton.me>
2023-11-18 20:43:06 +0800

    add another word to listener

--- events/come.js
+++ events/come.js
@@ -2,7 +2,10 @@ module.exports = {
   name: 'messageCreate',
   async execute(message) {
     if (message.author.bot) return;
-    if (message.content.toLowerCase().includes('come')) {
+    if (
+      message.content.toLowerCase().includes('come') ||
+      message.content.toLowerCase().includes('coming')
+    ) {
       await message.reply('no coming');
     }
   },
