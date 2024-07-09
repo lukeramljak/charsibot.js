59929f49744335595478ab01cbab53df295b181b
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-07-09 22:57:55 +0800

    Fix GuildMemberRemove message not sending

--- events/memberRemove.js
+++ events/memberRemove.js
@@ -1,13 +1,15 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
   name: Events.GuildMemberRemove,
   async execute(member) {
-    const channelId = '1018070065423335437';
+    const channelId = "1018070065423335437";
 
     const channel = member.guild.channels.cache.get(channelId);
     if (channel) {
-      channel.send(`${member.user.tag} has left the server.`);
+      await channel.send(
+        `${member.user.username} has left the server. <:rip:1057489640636035102>`
+      );
     }
   },
 };
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const dotenv = require('dotenv');
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const dotenv = require("dotenv");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.MessageContent,
   ],
 });
@@ -14,18 +15,18 @@ const client = new Client({
 dotenv.config();
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
     .readdirSync(commandsPath)
-    .filter((file) => file.endsWith('.js'));
+    .filter((file) => file.endsWith(".js"));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
+    if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
@@ -35,10 +36,10 @@ for (const folder of commandFolders) {
   }
 }
 
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
   .readdirSync(eventsPath)
-  .filter((file) => file.endsWith('.js'));
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
